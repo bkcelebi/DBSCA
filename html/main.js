619561ffba168a743205a1220ab07ef97d874e2a
bkcelebi <burakkcelebi@hotmail.com>
2022-04-30 12:22:44 +0100

    table fixed database has some errors

--- html/main.js
+++ html/main.js
@@ -1,49 +1,56 @@
 
 let baseURL='https://burak.dbsprojects.ie:8080/';
 
-// let doIt=() => {
-//     let tab=document.getElementById("tab1");
-//     let rows=tab1.getElementsByTagName('tr');
-//     fetch(baseURL)
-//     .then(response => response.json())
-//     .then(data=>data.Results.forEach(  //.slice(0,3)
-//         x => {
-//         let newRow =rows[0].cloneNode(true);
-//         let divs=newRow.getElementsByTagName('td');
-//         divs[0].innerHTML=x['ID'];
-//         divs[1].innerHTML=x['Name'];
-//         divs[2].innerHTML=x['Email'];
-//         tab1.appendChild(newRow);
+
+let doIt=()=>{
+    let tab=document.getElementById("tab1");
+    let rows=tab1.getElementsByTagName('tr');
+    fetch('https://burak.dbsprojects.ie:8080/')
+      .then(response => response.json())
+      .then(data=>data.Results.forEach(  //.slice(0,3)
+        x=>{
+          let newRow=rows[0].cloneNode(true);
+          let divs=newRow.getElementsByTagName('td');
+          divs[0].innerHTML=x['ID'];
+          divs[1].innerHTML=x['Name'];
+          divs[2].innerHTML=x['Email'];
+          tab1.appendChild(newRow);
+        }
+      )
+    );
+}
+
+window.onload = doIt;
+
+// let loadData = () => {
+//     fetch(baseURL).then(
+//         response => {
+//             response.json().then(
+//                 data => {
+//                     console.log(data);
+//                     if(data.length > 0){
+//                         var temp = "";
+//                         data.forEach((u) => {
+//                             temp += "<tr>";
+//                             temp += "<td>"+u.ID+"</td>";
+//                             temp += "<td>"+u.Name+"</td>";
+//                             temp += "<td>"+u.Email+"</td></tr>";
+//                         })
+
+//                         document.getElementById('tableBody').innerHTML = temp;
+//                     }
+//                 }  
+//             )
 //         }
 //     )
-//     );
 // }
 
-fetch(baseURL).then(
-    res => {
-        res.json().then(
-            data => {
-                console.log(data);
-                if(data.length > 0){
-                    var temp = "";
-                    data.forEach((u) => {
-                        temp += "<tr>";
-                        temp += "<td>"+u.ID+"</td>";
-                        temp += "<td>"+u.Name+"</td>";
-                        temp += "<td>"+u.Email+"</td></tr>";
-                    })
-
-                    document.getElementById('tableBody').innerHTML = temp;
-                }  
-            }
-        )
-    }
-)
-            
+// window.onload = loadData;
+ 
 
 let deleteStudent = () => {   
-    let deleteEnt = document.getElementById('deleteEnt').value;   
-    fetch(baseURL+'delete?id='+deleteEnt)
+    let deleteEntr = document.getElementById('deleteEnt').value;   
+    fetch(baseURL+'delete?id='+deleteEntr)
     .then((resp) => {
         location.reload();
         alert("Student Deleted");
@@ -67,7 +74,7 @@ let addStudent = () => {
     let studentMail = document.getElementById('studentMail').value;
     fetch(baseURL+'add?id='+studentId+'&name='+studentName+'&email='+studentMail)
     .then((resp) => {
-        location.reload();
         alert("Student Added");
+        location.reload();      
     });
 }
